534f5464d21d3b7ff5485422169e31366ddb1c41
orangemug <info@orangemug.co.uk>
2016-03-12 13:25:05 +0000

    Fix for js API to store in key chain

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var core = require("./core");
-var prompt = require("prompt-sync")();
+var prompt = require("./bin/prompt");
 var fs = require("fs");
 
 var env = process.env.NODE_ENV || "development";
@@ -10,7 +10,7 @@ module.exports = function(filepath, password) {
 
   if(!password) {
     if(env === "development") {
-      password = prompt("Passpharse to decrypt config: ", {echo: "*"});
+      password = prompt("Passpharse to decrypt config: ", filepath);
     } else {
       throw "No password to decrypt config"
     }
